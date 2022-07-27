94665c103addbc50c2a05708664f81c22997edef
Emilio Benavides <emilio.benavides68@gmail.com>
2022-07-27 16:48:16 -0500

    added global css folder

--- feedback-app/src/index.js
+++ feedback-app/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import  ReactDom from 'react-dom'
+import App from "./App";
 
-ReactDom.render(<h1>My App</h1>, document.getElementById('root'))
+ReactDom.render( <React.StrictMode> <App /></React.StrictMode> , document.getElementById('root'))
