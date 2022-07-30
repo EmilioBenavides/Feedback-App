aa9b0272d4d2dd83405dde74fd15812031cad4fa
Emilio Benavides <emilio.benavides68@gmail.com>
2022-07-30 15:59:20 -0500

    added feedbackitem component

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+    id: 1,
+    rating: 10,
+    text: 'lorem ipsom sdfsdg  wogwidj weiu fgwiygf owuygfwo  weiygf dgyf  gwiudfg pisgf wiufg siduf gi isdgf iwgwf ius is  isugf iusgf',
+},
+    {
+        id: 2,
+        rating: 9,
+        text: 'lorem ipsom sdfsdg  wogwidj weiu fgwiygf owuygfwo  weiygf dgyf  gwiudfg pisgf wiufg siduf gi isdgf iwgwf ius is  isugf iusgf',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'lorem ipsom sdfsdg  wogwidj weiu fgwiygf owuygfwo  weiygf dgyf  gwiudfg pisgf wiufg siduf gi isdgf iwgwf ius is  isugf iusgf',
+    },
+]
+
+export default FeedbackData
