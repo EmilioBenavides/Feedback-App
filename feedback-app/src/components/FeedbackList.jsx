50e40b6d3ce3f7e5ce7683475a0d1bc257a47c46
Emilio Benavides <emilio.benavides68@gmail.com>
2022-08-03 18:04:39 -0500

    added aboutpage

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -2,9 +2,10 @@ import PropTypes from "prop-types"
 import FeedbackItem from "./FeedbackItem"
 
 function FeedbackList({feedback, handleDelete}) {
-    if(!feedback || feedback.length === 0) {
+    if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet</p>
     }
+
     return (
         <div className={'feedback-list'}>
             {feedback.map((item) => (
